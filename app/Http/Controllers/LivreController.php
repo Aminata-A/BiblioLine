4c61beb77144f0a1bcdcb4364c2278acfe0d9c0e
Aminata-A <aminataassane.ndiaye@gmail.com>
2024-06-06 15:47:45 +0000

    mise en place de la route pour la page d'accueil , et controlleur pour afficher les element de la table

--- app/Http/Controllers/LivreController.php
+++ app/Http/Controllers/LivreController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Livre;
 use Illuminate\Http\Request;
 
 class LivreController extends Controller
 {
-    //
+    public function accueil(){
+        $livres = Livre::All(); // Récupère les 3 premiers livres
+        return view('livres.accueil', compact('livres')); 
+    }
 }
